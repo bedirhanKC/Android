2a46f8721ecdc38d1dadf5c5eae1dca7c9dadc1d
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2024-08-13 02:21:19 +0300

    Add files via upload

---
+++ L3 - Wiew Binding/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.bedirhankc.l3_viewbinding
+
+import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bedirhankc.l3_viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+    }
+}
